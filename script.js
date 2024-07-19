1f3b5f8ebe4ed12bf23d31649a0a6f038672bd60
Zub <zub.ingco@gmail.com>
2024-07-19 09:28:55 +0700

    Update Script

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ async function checkReferrer() {
     const phoneNumber = document.getElementById('checkReferrerInput').value;
     const response = await fetch(`${scriptUrl}?action=checkReferrer&phoneNumber=${phoneNumber}`);
     const result = await response.json();
+    console.log(result);
     document.getElementById('checkReferrerResult').textContent = result ? 'Referrer code exists.' : 'Referrer code does not exist.';
 }
 
@@ -11,6 +12,7 @@ async function addReferrer() {
     const phoneNumber = document.getElementById('newReferrerInput').value;
     const response = await fetch(`${scriptUrl}?action=addReferrer&phoneNumber=${phoneNumber}`);
     const result = await response.text();
+    console.log(result);
     document.getElementById('addReferrerResult').textContent = result;
 }
 
@@ -18,6 +20,7 @@ async function addReferred() {
     const phoneNumber = document.getElementById('newReferredInput').value;
     const response = await fetch(`${scriptUrl}?action=addReferred&phoneNumber=${phoneNumber}`);
     const result = await response.text();
+    console.log(result);
     document.getElementById('addReferredResult').textContent = result;
 }
 
@@ -25,5 +28,6 @@ async function checkReferred() {
     const phoneNumber = document.getElementById('checkReferredInput').value;
     const response = await fetch(`${scriptUrl}?action=checkReferred&phoneNumber=${phoneNumber}`);
     const result = await response.json();
+    console.log(result);
     document.getElementById('checkReferredResult').textContent = result ? 'Referred code exists.' : 'Referred code does not exist.';
 }
